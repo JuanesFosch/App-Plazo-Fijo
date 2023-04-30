f541f96a5591c1c4df5d644f80eb36f31e8e692f
JuanesFosch <juan.fosch@gmail.com>
2023-04-30 03:28:56 -0300

    Código refactorizado

--- BCRA_selenium_Refactor.py
+++ BCRA_selenium_Refactor.py
@@ -3,42 +3,19 @@ from selenium.webdriver.common.by import By #---------Importa el método 'By' pa
 import time #---------Sirve para dar un intervalo de tiempo a las ventanas para que se mantengan abiertas.
 import requests #---------Sirve para 'pedir' información, como la URL de un sitio.
 import pandas as pd 
-from sqlalchemy import create_engine #---------Sirve para exportar un DataFrame de Pandas a una base de datos MySQL.
-
-
-#------HABRÍA QUE HACER UN BUCLE PARA QUE BUSQUE TODAS LAS POSIBILIDADES POR TEXTO: BANCOS, UVA Y CER.----
-#----LA FECHA VA A SER SIEMPRE LA MISMA.-----
-#----------HABRÍA QUE HACER OTRO BUCLE PARA QUE BUSQUE LA TABLA (QUE ES SIEMPRE LA MISMA) PERO QUE RENOMBRE SEGÚN CATEGORÍA-----
-#----EL OTRO BUCLE ES DE AGARRAR CADA DF, FORMATEARLO E IMPRIMIRLO----
-
+#from sqlalchemy import create_engine #---------Sirve para exportar un DataFrame de Pandas a una base de datos MySQL.
 
 
 options = webdriver.ChromeOptions()
-
-options.add_experimental_option('excludeSwitches', ['enable-logging']) # Esto se hace para que la ventana del navegador se mantenga abierta.
-
+# Lo siguiente se hace para que la ventana del navegador se mantenga abierta.
+options.add_experimental_option('excludeSwitches', ['enable-logging']) 
 driver = webdriver.Chrome(options=options)
-
 
 #-----------Inicio de la interacción con el sitio web.
 
 driver.get("https://www.bcra.gob.ar/Estadisticas/EstadisSitiopublico/PreInicio.aspx")
-
 title =driver.title
-
 driver.implicitly_wait(30)
-
-#Id de cada checkbox en la búsqueda por TreeView
-id_ver="btnAcept"
-id_tasas= "TreeView1n52CheckBox"
-id_bancos="TreeView1n59CheckBox"
-id_CER= "TreeView1n166CheckBox"
-id_UVA= ""
-
-# Números para la búsqueda por número de serie.
-n_serie_bancos= 1222
-n_serie_CER= 3540
-n_serie_UVA=7913
 
 # Valores para la búsqueda por texto.
 términos_de_búsqueda={'bancos_privados':["Bancos Privados","listaSeries_ctl02_chkSeleccion"], # Texto y Id de cada checkbox.
@@ -49,12 +26,6 @@ términos_de_búsqueda={'bancos_privados':["Bancos Privados","listaSeries_ctl02_
 f_desde="01/02/2023" # Deberían ser un campo 'Input'
 f_hasta="01/03/2023"
 
-nombres_columnas1= {"bancos_original": 'Tasas de interés - Por depósitos - Series diarias - BADLAR - Tasas de interés por depósitos a plazo fijo de 30 a 35 días de plazo - Depósitos de más de un millón de pesos, en porcentaje nominal anual - Bancos privados' 
-            , "bancos_nuevo": 'Badlar',
-            "cer_original": 'Tasas de interés - Tasas de interés y coeficientes de ajuste establecidos por el BCRA - Coeficiente de estabilización de referencia (CER), serie diaria',
-            "cer_nuevo": 'CER',
-            "uva_original": 'Unidad de Valor Adquisitivo (UVA)- en pesos - base 31.3.2016=14.05',
-            "uva_nuevo": 'UVA' }
 
 nombres_columnas= {"bancos": ['Tasas de interés - Por depósitos - Series diarias - BADLAR - Tasas de interés por depósitos a plazo fijo de 30 a 35 días de plazo - Depósitos de más de un millón de pesos, en porcentaje nominal anual - Bancos privados', 
             'Badlar'],
@@ -64,17 +35,8 @@ nombres_columnas= {"bancos": ['Tasas de interés - Por depósitos - Series diari
 # Si se busca por número de serie todos a la vez, las checkbox repiten los nombres, entonces no sería correcto.
 # Hay que buscar por texto, y ahí es seguro el nombre de la checkbox que se genera. Los números de serie sirven para ubicar visualmente cada checkbox en el HTML.
 
-#--------Dataframe de Bancos Privados.
 
 #-------------Búsqueda por término. (PUEDE SER UNA FUNCIÓN)-----
-"""
-bancos = driver.find_element(by= By.ID, value="tBusqueda")
-bancos.send_keys(términos_de_búsqueda['bancos_privados'][0])
-ver_por_término= driver.find_element(By.ID,value="btnBuscar") 
-ver_por_término.click()
-check_bancos= driver.find_element(By.ID,value=términos_de_búsqueda['bancos_privados'][1])
-check_bancos.click() """
-
 
 def búsqueda_por_texto(término):
     """Busca el campo para ingresar términos para la búsqueda e ingresa el término"""
@@ -85,25 +47,13 @@ def búsqueda_por_texto(término):
     check_categoría= driver.find_element(By.ID,value=términos_de_búsqueda[f'{término}'][1])
     check_categoría.click()
 
-flag = 'bancos_privados'
-búsqueda_por_texto(flag)
 
-#print(flag)
+#--------Aceptar selección---
+def aceptar_selección():
+    ver=driver.find_element(By.ID,value="btnAcept")
+    ver.click()
 
-
-#--------Aceptar selección---(PODRÍA SER UNA FUNCIÓN PARA TODOS LOS CLICKEOS)
-ver=driver.find_element(By.ID,value="btnAcept")
-ver.click()
-
-#-------Llenar campos de fecha. (TIENE QUE SER UNA FUNCIÓN SÍ O SÍ)----
-"""
-fecha_desde=driver.find_element(By.ID,value="fDesde")
-fecha_desde.clear()
-fecha_desde.send_keys(f_desde)
-
-fecha_hasta=driver.find_element(By.ID,value="fHasta")
-fecha_hasta.clear()
-fecha_hasta.send_keys(f_hasta)  """
+#-------Llenar campos de fecha----
 
 def selección_fecha(desde,hasta):
     """Busca el campo para ingresar fechas e ingresa las fechas"""
@@ -115,15 +65,8 @@ def selección_fecha(desde,hasta):
     fecha_hasta.clear()
     fecha_hasta.send_keys(hasta)
 
-selección_fecha(f_desde,f_hasta)
-#------Aceptar selección de fecha. (TIENE QUE SER UNA FUNCIÓN SÍ O SÍ)----
-"""
-ultimo_ver=driver.find_element(By.ID,value="btnAcept")
-beforePopup= driver.window_handles
-ultimo_ver.click()
-afterPopup = driver.window_handles
-afterPopup.remove(afterPopup[0])
-driver.switch_to.window(afterPopup[0]) """
+
+#------Aceptar selección de fecha.----
 
 def aceptar_fecha(botón):
     """Acepta la selección de fecha anterior"""
@@ -134,19 +77,9 @@ def aceptar_fecha(botón):
     afterPopup.remove(afterPopup[0])
     driver.switch_to.window(afterPopup[0])
 
-
-aceptar_fecha("btnAcept")
-
 #--------Busca la tabla donde están las columnas y filas con los datos. (PUEDE SER UNA FUNCIÓN)--
 
 # Método usando 'requests' y 'read_html'.
-"""
-req=requests.get(driver.current_url)
-table = pd.read_html(req.text,attrs={'id':'GrdResultados'},encoding='utf-8')
-
-df_categoría = table[0] # Este índice de la tabla de HTML es el DataFrame.
-df_categoría.rename(columns={'Tasas de interés - Por depósitos - Series diarias - BADLAR - Tasas de interés por depósitos a plazo fijo de 30 a 35 días de plazo - Depósitos de más de un millón de pesos, en porcentaje nominal anual - Bancos privados': 'Badlar'}, inplace=True)
-"""
 
 def busca_tabla(original,nuevo):
     """Busca la tabla donde están las columnas y filas con los datos. Renombra las columnas"""
@@ -160,42 +93,59 @@ def busca_tabla(original,nuevo):
 
     df_categoría.rename(columns={f"{original}" : f"{nuevo}"}, inplace=True)   # Reemplaza la columna con el texto LITERAL indicado (original)
 
-    #print(df_categoría)
     return df_categoría
 
+dfs=[]   # Lista vacía para llenar con los DataFrame de cada categoría que se van a crear.
+flag = 'bancos_privados'    # Bandera para cambiar de categoría.
 
-
-if flag == 'bancos_privados':
+while flag == 'bancos_privados':
+    búsqueda_por_texto(flag)
+    aceptar_selección()
+    selección_fecha(f_desde,f_hasta)
+    aceptar_fecha("btnAcept")
     nombre_original= nombres_columnas["bancos"][0]
     nombre_nuevo= nombres_columnas["bancos"][1]
-elif flag == 'CER':
+    df_bancos = busca_tabla(nombre_original,nombre_nuevo)
+    dfs.append(df_bancos)
+    #print(df_bancos)
+    flag = 'CER'
+
+while flag == 'CER':
+    driver.get("https://www.bcra.gob.ar/Estadisticas/EstadisSitiopublico/PreInicio.aspx")
+    title =driver.title
+    driver.implicitly_wait(30)
+    búsqueda_por_texto(flag)
+    aceptar_selección()
+    selección_fecha(f_desde,f_hasta)
+    aceptar_fecha("btnAcept")
     nombre_original= nombres_columnas["cer"][0]
     nombre_nuevo= nombres_columnas["cer"][1]
-elif flag == 'UVA':
+    df_cer= busca_tabla(nombre_original,nombre_nuevo)
+    dfs.append(df_cer)
+    #print(df_cer)
+    flag = 'UVA'
+
+while flag == 'UVA':
+    driver.get("https://www.bcra.gob.ar/Estadisticas/EstadisSitiopublico/PreInicio.aspx")
+    title =driver.title
+    driver.implicitly_wait(30)
+    búsqueda_por_texto(flag)
+    aceptar_selección()
+    selección_fecha(f_desde,f_hasta)
+    aceptar_fecha("btnAcept")
     nombre_original= nombres_columnas["uva"][0]
     nombre_nuevo= nombres_columnas["uva"][1]
-    #print(nombre_original,nombre_nuevo)
-resultado = busca_tabla(nombre_original,nombre_nuevo)
+    df_uva= busca_tabla(nombre_original,nombre_nuevo)
+    dfs.append(df_uva)
+    #print(df_uva)
+    driver.close()
+    flag= ""
 
+#-----Solucionar problemas de caracteres raros.--TENER EN CUENTA EL CASO LOS NOMBRES DE COLUMNAS DE UVA---
+#---Limpieza y revisión del dataframe. ----
 
-print(resultado)
-if resultado.columns[1] == 'Badlar':
-    flag = 'CER'
-#búsqueda_por_texto(flag)
-print(flag)
-búsqueda_por_texto('flag')
-print(resultado)  
-"""for key in términos_de_búsqueda.keys():
-    flag= key
-búsqueda_por_texto(flag)            
-"""
-  
-#---TENDRÍA QUE HACER UNA LISTA DE LOS QUE YA SE PASARON O ALGO ASÍ---
-"""
-#-----Solucionar problemas de caracteres raros. (PUEDE SER UNA FUNCIÓN)--TENER EN CUENTA EL CASO LOS NOMBRES DE COLUMNAS DE UVA---
-#---TIENE QUE DEVOLVER 3 DF POR SEPARADO, PARA TRATARLOS DESPUES Y JUNTARLOS---
 def formatos(df_categoría):
-    #Solucionar problemas de caracteres raros
+    """Solucionar problemas de caracteres raros"""
 
     df_categoría['Fecha']=df_categoría['Fecha'].str.encode('ascii', errors='ignore')
 
@@ -212,28 +162,28 @@ def formatos(df_categoría):
         df_categoría['Badlar']=df_categoría['Badlar'].str.decode("utf-8")
     if df_categoría.columns[1] == 'CER':
         df_categoría['CER']=df_categoría['CER'].str.decode("utf-8")
-    if df_categoría.columns[1] == 'CER':
-        df_categoría['CER']=df_categoría['CER'].str.decode("utf-8")
+    if df_categoría.columns[1] == 'UVA':
+        df_categoría['UVA']=df_categoría['UVA'].str.decode("utf-8")
     df_categoría.replace('&nbsp','',regex=True,inplace=True)
     df_categoría.drop([0],inplace=True)
-# Limpieza y revisión del dataframe. (TIENE QUE SER UNA FUNCIÓN SÍ O SÍ)----
 
-print(df_categoría.head())
-print(df_categoría.info())
-    
+    return df_categoría
+
+# Construcción de los 3 dataframe. (TIENE QUE SER UNA FUNCIÓN SÍ O SÍ)----
+
+flag_df= "bancos"
+while flag_df == "bancos":
+    df_bancos=formatos(dfs[0])
+    flag_df = "cer"
+while flag_df == "cer":
+    df_cer=formatos(dfs[1])
+    flag_df = "uva"
+
+while flag_df == "uva":
+    df_uva=formatos(dfs[2])
+    flag_df = ""
 
 
-formatos(df_categoría) """
-# Exportar como csv.
-#df.to_csv('datos.csv',encoding='utf-8',index=False)
-#time.sleep(5)
-
-
-
-
-
-
-"""
 #------------Unión de todos los Dataframe.
 
 df_completo= df_bancos.set_index('Fecha').join([df_cer.set_index('Fecha'),df_uva.set_index('Fecha')])
@@ -241,8 +191,7 @@ df_completo= df_bancos.set_index('Fecha').join([df_cer.set_index('Fecha'),df_uva
 print(df_completo.head(15))
 #print(df_completo.info())
 
-# Exportar como csv. 
-#df_completo.to_csv('datos_completo.csv',encoding='utf-8',index=True)
+#----Correción de las fechas faltantes por falta de publicación diaria de Badlar.
 
 #fechas=pd.date_range(start="01/02/2023",end="01/03/2023",freq='D')
 fechas1=pd.to_datetime([f_desde,f_hasta],format='%d/%m/%Y',errors='ignore')
@@ -252,8 +201,7 @@ fec=pd.DataFrame(fechas,columns=['Fecha'])
 fec['Fecha']=fec['Fecha'].dt.strftime('%d/%m/%Y')
 
 df_completo= fec.set_index('Fecha').join([df_bancos.set_index('Fecha'),df_cer.set_index('Fecha'),df_uva.set_index('Fecha')])
-#print(df_completo)
-#df_completo.to_csv('datos_completo.csv',encoding='utf-8',index=True)
-#print(fec)
+print(df_completo.head(30))
 
-"""
+#----Exportar como csv. 
+#df_completo.to_csv('datos_completo.csv',encoding='utf-8',index=True)
